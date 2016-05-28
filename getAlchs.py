8d5704c6f37729bd0bf309592d2ac3eb78b22890
JakeStraker <jake.straker@live.co.uk>
2016-05-28 15:34:45 +0100

    Efficiency and comments

--- getAlchs.py
+++ getAlchs.py
@@ -7,10 +7,10 @@ nat = data['561']['sell_average']
 #print nat
 for item in data:
 	name = data[item]['name']
-	alchPrice = math.trunc(0.6*(data[item]['sp']))
+	alchPrice = math.trunc(0.6*(data[item]['sp'])) #confirmed by devs alch price is trunc(0.6*Shop price) 
 	price = data[item]['sell_average']
 	profit = alchPrice-(price + nat)
-	message = (name, " buy for: ", str(price), " and Gain/Lose = ", str(profit))
 	#print ''.join(message)
 	if (profit > -15 and price > 0):
+		message = (name, " buy for: ", str(price), " and Gain/Lose = ", str(profit)) #more efficient to have it here
 		print ''.join(message)
