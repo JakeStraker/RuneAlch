aa555dfdc51b09d51b439acfd96c2a4fc500cfa7
JakeStraker <jake.straker@live.co.uk>
2016-05-28 15:33:03 +0100

    Structural Change

--- getAlchs.py
+++ getAlchs.py
@@ -9,7 +9,8 @@ for item in data:
 	name = data[item]['name']
 	alchPrice = math.trunc(0.6*(data[item]['sp']))
 	price = data[item]['sell_average']
-	message = (name, " Alchs for ", str(alchPrice), " bought for: ", str(price))
+	profit = alchPrice-(price + nat)
+	message = (name, " buy for: ", str(price), " and Gain/Lose = ", str(profit))
 	#print ''.join(message)
-	if ((alchPrice-(price + nat)) > -15 and price > 0):
+	if (profit > -15 and price > 0):
 		print ''.join(message)
